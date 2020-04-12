109548360228b3e014a17a65f0b6a17065590da8
AMWOLLO VICTOR <amwollo@yahoo.com>
2020-04-12 14:37:59 +0300

    Calculate severeCasesByRequestedTime

--- src/estimator.php
+++ src/estimator.php
@@ -25,42 +25,14 @@ function covid19ImpactEstimator($data)
       $_factor = ($timeToElapse * 30) / 3;
       $_days = $timeToElapse * 30;
   }
-  
+
   $response['impact']['infectionsByRequestedTime'] = (int)($data['reportedCases'] * 10 * pow(2,$_factor));
   $response['severeImpact']['infectionsByRequestedTime'] = (int)($data['reportedCases'] * 50 * pow(2,$_factor));
 
+  $response['impact']['severeCasesByRequestedTime'] = (int)($response['impact']['infectionsByRequestedTime'] * 15/100);
+  $response['severeImpact']['severeCasesByRequestedTime'] = (int)($response['severeImpact']['infectionsByRequestedTime'] * 15/100);
+
+  
+
   return $response;
-}
-
-
-
-
-
-
-
-
-function daysFactor($periodType,$timeToElapse)
-{
-  switch ($periodType) {
-    case 'days':
-      
-      break;
-
-    case 'weeks':
-      $_factor = ($timeToElapse * 7) / 3;
-      $_days = $timeToElapse * 7;
-      break;
-    
-    case 'months':
-      $_factor = ($timeToElapse * 30) / 3;
-      $_days = $timeToElapse * 30;
-      break;
-    
-    default:
-      $_factor = 0;
-      $_days = 0;
-      break;
-
-    return $arrayName = array('' => , );;
-  }
 }
