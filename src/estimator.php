40a898b8e8feb0afe677911c2a57972757d11a92
AMWOLLO VICTOR <amwollo@yahoo.com>
2020-04-12 15:34:40 +0300

    Code cleanup

--- src/estimator.php
+++ src/estimator.php
@@ -2,7 +2,7 @@
 
 function covid19ImpactEstimator($data)
 {
-  $data = json_decode($data, true);
+  // $data = json_decode($data, true);
   $response = array();
 
   $response['impact']['currentlyInfected'] = (int)$data['reportedCases'] * 10;
