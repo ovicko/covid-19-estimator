f8935d80660e76a46dfc6135ae17ac5dab2165ae
AMWOLLO VICTOR <amwollo@yahoo.com>
2020-04-14 03:55:15 +0300

    change  type to integer

--- src/estimator.php
+++ src/estimator.php
@@ -12,25 +12,24 @@ function covid19ImpactEstimator($data)
   $timeToElapse = $data['timeToElapse'];
 
   if ($data['periodType'] === 'days') {
-    $_factor = $timeToElapse / 3;
-    $_days = $timeToElapse;
+    $elapsedDays = $timeToElapse;
   }
 
   if ($data['periodType'] === 'weeks') {
-      $_factor = ($timeToElapse * 7) / 3;
-      $_days = $timeToElapse * 7;
+      $elapsedDays = $timeToElapse * 7;
   }
 
   if ($data['periodType'] === 'months') {
-      $_factor = ($timeToElapse * 30) / 3;
-      $_days = $timeToElapse * 30;
+    $elapsedDays = $timeToElapse * 30;
   }
+
+  $_factor = intdiv($elapsedDays,3);
 
   $response['impact']['infectionsByRequestedTime'] = (int)($data['reportedCases'] * 10 * pow(2,$_factor));
   $response['severeImpact']['infectionsByRequestedTime'] = (int)($data['reportedCases'] * 50 * pow(2,$_factor));
 
-  $response['impact']['severeCasesByRequestedTime'] = (int)($response['impact']['infectionsByRequestedTime'] * 15/100);
-  $response['severeImpact']['severeCasesByRequestedTime'] = (int)($response['severeImpact']['infectionsByRequestedTime'] * 15/100);
+  $response['impact']['severeCasesByRequestedTime'] = (int)($response['impact']['infectionsByRequestedTime'] * 0.15);
+  $response['severeImpact']['severeCasesByRequestedTime'] = (int)($response['severeImpact']['infectionsByRequestedTime'] * 0.15);
 
   $response['impact']['hospitalBedsByRequestedTime'] = (int)(($data['totalHospitalBeds'] * 35/100) - $response['impact']['severeCasesByRequestedTime']);
   $response['severeImpact']['hospitalBedsByRequestedTime'] = (int)(($data['totalHospitalBeds'] * 35/100) - $response['severeImpact']['severeCasesByRequestedTime']);
@@ -44,8 +43,8 @@ function covid19ImpactEstimator($data)
   $_avgDailyIncomeInUSD = $data['region']['avgDailyIncomeInUSD'];
   $_avgDailyIncomePopulation = $data['region']['avgDailyIncomePopulation'];
 
-  $response['impact']['dollarsInFlight'] = (int)( ($response['impact']['infectionsByRequestedTime'] * $_avgDailyIncomePopulation  * $_avgDailyIncomeInUSD ) / $_days );
-  $response['severeImpact']['dollarsInFlight'] = (int)(($response['severeImpact']['infectionsByRequestedTime'] * $_avgDailyIncomePopulation  * $_avgDailyIncomeInUSD ) / $_days );
+  $response['impact']['dollarsInFlight'] = (int)( ($response['impact']['infectionsByRequestedTime'] * $_avgDailyIncomePopulation  * $_avgDailyIncomeInUSD ) / $elapsedDays );
+  $response['severeImpact']['dollarsInFlight'] = (int)(($response['severeImpact']['infectionsByRequestedTime'] * $_avgDailyIncomePopulation  * $_avgDailyIncomeInUSD ) / $elapsedDays );
 
   return $response;
 }
